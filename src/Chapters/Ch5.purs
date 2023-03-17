5b11779c0e952b9286f747801e7bad40ed3592b5
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2023-03-17 13:51:23 +0100

    all exercises of Chapter 5 are done

--- src/Chapters/Ch5.purs
+++ src/Chapters/Ch5.purs
@@ -5,7 +5,13 @@ module Ch5
   , ($)
   , apply
   , applyFlipped
+  , catMaybes
+  , concat
   , const
+  , drop
+  , dropEnd
+  , dropWhile
+  , filter
   , findIndex
   , findLastIndex
   , flip
@@ -15,19 +21,24 @@ module Ch5
   , last
   , length
   , null
+  , range
   , reverse
   , singleton
   , snoc
   , tail
+  , take
+  , takeEnd
+  , takeWhile
   , test
   , uncons
   ) where
 
 import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..), snd)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, discard, negate, otherwise, show, (+), (-), (<), (>=), (/=), (==), type (~>))
+import Prelude (type (~>), Unit, discard, max, negate, otherwise, show, (+), (-), (/=), (<), (>), (==), (>=), (<=), (<<<), (>>>))
 
 apply :: ∀ a b. (a -> b) -> a -> b
 apply f x = f x
@@ -145,8 +156,118 @@ reverse Nil = Nil
 reverse (x : Nil) = (x : Nil)
 reverse (x : xs) = snoc (reverse xs) x
 
+{- | Takes a list of lists and returns a single list containing all elements 
+     of all lists in the same order.
+-}
+concat :: ∀ a. List (List a) -> List (a)
+concat Nil = Nil
+concat (x : Nil) = x
+{- case of an empty list in our list of lists, 
+   and case of when we have exhausted the head of our list of lists: at the end of each list, there is a Nil
+-}
+concat (Nil : xss) = concat xss
+concat ((x : xs) : xss) = x : concat (xs : xss)
+
+{- | Filters out elements of the specified list for which the specified predicate does not hold.
+-}
+filter :: ∀ a. (a -> Boolean) -> List a -> List a
+
+-- Simple implementation, time-efficient, but not space/memory-efficient
+-- filter _ Nil = Nil
+-- filter predicate (x : xs)
+--   | predicate x = x : filter predicate xs
+--   | otherwise = filter predicate xs
+
+-- A tail-recursive implementation: space/memory-efficient, but not time-efficient because of the reversal of the resulting list
+filter predicate = reverse <<< go Nil
+  where
+  go filtered Nil = filtered
+  go filtered (x : xs)
+    | predicate x = go (x : filtered) xs
+    | otherwise = go filtered xs
+
+{- | Takes a list of Maybe's, filters out the Nothings and unwraps the Just a's, returning a list of present a's.
+-}
+catMaybes :: ∀ a. List (Maybe a) -> List a
+-- Not tail-recursive:
+catMaybes Nil = Nil
+catMaybes (x : xs) = case x of
+  Just y -> y : catMaybes xs
+  Nothing -> catMaybes xs
+
+{- | Creates a list with a sequential set of numbers.
+-}
+range :: Int -> Int -> List Int
+range from to = go Nil to from --here we go from to to from so that the cons infra will create the list in the right order
+  where
+  go rl start end
+    | start == end = start : rl
+    | otherwise = go (start : rl) (start - step) end
+  step = if from < to then 1 else -1
+
+take :: ∀ a. Int -> List a -> List a
+-- Not tail recursive:
+-- take _ Nil = Nil
+-- take n (x : xs)
+--   | (n <= 0) = Nil
+--   | otherwise = x : take (n - 1) xs
+
+-- Tail recursive:
+take n = reverse <<< go Nil (max 0 n) -- max 0 n makes sure that the quantity to take is positive
+  where
+  go nl _ Nil = nl
+  go nl 0 _ = nl
+  go nl q (x : xs) = go (x : nl) (q - 1) xs
+
+{- | Removes the specified amount of elements from the specified list.
+-}
+drop :: ∀ a. Int -> List a -> List a
+drop _ Nil = Nil
+drop n (x : xs)
+  | n <= 0 = (x : xs)
+  | otherwise = drop (n - 1) xs
+
+takeWhile :: ∀ a. (a -> Boolean) -> List a -> List a
+takeWhile predicate = reverse <<< go Nil
+  where
+  go acc Nil = acc
+  go acc (x : xs)
+    | predicate x = go (x : acc) xs
+    | otherwise = acc
+
+dropWhile :: ∀ a. (a -> Boolean) -> List a -> List a
+dropWhile _ Nil = Nil
+dropWhile predicate l@(x : xs)
+  | predicate x = dropWhile predicate xs
+  | otherwise = l
+
+takeEnd :: ∀ a. Int -> List a -> List a
+takeEnd n = go >>> snd
+  where
+  go Nil = Tuple 0 Nil
+  go (x : xs) = go xs # \tup@(Tuple c nl) -> if c < n then Tuple (c + 1) (x : nl) else tup
+
+-- equivalent to line above, just using $ instead of #: 
+-- go (x : xs) = (\tup@(Tuple c nl) -> if c < n then Tuple (c + 1) (x : nl) else tup) $ go xs
+
+dropEnd :: ∀ a. Int -> List a -> List a
+dropEnd n = go >>> snd
+  where
+  go Nil = Tuple 0 Nil
+  go (x : xs) = go xs # \(Tuple c nl) -> Tuple (c + 1) $ if c < n then nl else x : nl
+
+zip :: ∀ a b. List a -> List b -> List (Tuple a b)
+zip _ Nil = Nil
+zip Nil _ = Nil
+zip (x : xs) (y : ys) = Tuple x y : zip xs ys
+
+unzip :: ∀ a b. List (Tuple a b) -> Tuple (List a) (List b)
+unzip Nil = Tuple Nil Nil
+unzip (Tuple x y : ts) = unzip ts # (\(Tuple xs ys) -> Tuple (x : xs) (y : ys))
+
 test :: Effect Unit
 test = do
+
   log (show (flip const 1 2))
   log $ show $ flip const 1 2
   flip const 1 2 # show # log
@@ -194,4 +315,40 @@ test = do
   log $ show $ findLastIndex (_ == 10) (11 : 12 : Nil)
 
   log $ show $ reverse (10 : 20 : 30 : Nil)
-  
+
+  log $ show $ concat ((1 : 2 : 3 : Nil) : (4 : 5 : Nil) : (6 : Nil) : (Nil) : Nil)
+
+  log $ show $ filter (4 > _) (1 : 2 : 3 : 4 : 5 : 6 : Nil)
+
+  log $ show $ catMaybes (Just 1 : Nothing : Just 2 : Nothing : Nothing : Just 5 : Nil)
+
+  log $ show $ range 1 10
+  log $ show $ range 3 (-3)
+  log $ show $ range 3 3
+
+  log $ show $ take 5 (12 : 13 : 14 : Nil)
+  log $ show $ take 5 (-7 : 9 : 0 : 12 : -13 : 45 : 976 : -19 : Nil)
+
+  log $ show $ drop 2 (1 : 2 : 3 : 4 : 5 : 6 : 7 : Nil)
+  log $ show $ drop (-2) (1 : 2 : 3 : 4 : 5 : 6 : 7 : Nil)
+  log $ show $ drop 10 (Nil :: List Unit)
+
+  log $ show $ takeWhile (_ > 3) (5 : 4 : 3 : 99 : 101 : Nil)
+  log $ show $ takeWhile (_ == -17) (1 : 2 : 3 : Nil)
+
+  log $ show $ dropWhile (_ > 3) (5 : 4 : 3 : 99 : 101 : Nil)
+  log $ show $ dropWhile (_ == -17) (1 : 2 : 3 : Nil)
+
+  log $ show $ takeEnd 3 (1 : 2 : 3 : 4 : 5 : 6 : Nil)
+  log $ show $ takeEnd 10 (1 : Nil)
+
+  log $ show $ dropEnd 3 (1 : 2 : 3 : 4 : 5 : 6 : Nil)
+  log $ show $ dropEnd 10 (1 : Nil)
+
+  log $ show $ zip (1 : 2 : 3 : Nil) ("a" : "b" : "c" : "d" : "e" : Nil)
+  log $ show $ zip ("a" : "b" : "c" : "d" : "e" : Nil) (1 : 2 : 3 : Nil)
+  log $ show $ zip (Nil :: List Unit) (1 : 2 : Nil)
+
+  log $ show $ unzip (Tuple 1 "a" : Tuple 2 "b" : Tuple 3 "c" : Nil)
+  log $ show $ unzip (Tuple "a" 1 : Tuple "b" 2 : Tuple "c" 3 : Nil)
+  log $ show $ unzip (Nil :: List (Tuple Unit Unit))
